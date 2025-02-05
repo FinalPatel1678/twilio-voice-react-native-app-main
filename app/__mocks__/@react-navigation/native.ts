ed8d522f278ade1e6139a54922212ad3d487c118
final-patel1603 <fpatel@in.allmysons.com>
2025-02-05 09:59:14 +0530

    chore: remove test files

--- app/__mocks__/@react-navigation/native.ts
+++
@@ -1,6 +0,0 @@
-export const mockNavigationContainerRef = {
-  isReady: jest.fn().mockReturnValue(false),
-};
-export const createNavigationContainerRef = jest
-  .fn()
-  .mockReturnValue(mockNavigationContainerRef);
